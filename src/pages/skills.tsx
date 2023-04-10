9f84e9596e87cb77ebedd847090ecb46ef0a2d21
hirakuuuu <m.hiraku1129@gmail.com>
2023-04-10 18:13:49 +0900

    [add] 各種ページ追加

---
+++ src/pages/skills.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/organisms/Header";
+import React from "react";
+
+const Skills = () => {
+  return (
+    <>
+      <div>Skills</div>
+    </>
+  );
+};
+
+export default Skills;
