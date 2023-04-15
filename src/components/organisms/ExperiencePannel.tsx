2a6e6e79b7e0a6dc09821f79c65995de633c4b9e
hirakuuuu <m.hiraku1129@gmail.com>
2023-04-15 18:29:29 +0900

    [fix] deg付けないとダメなのか

--- src/components/organisms/ExperiencePannel.tsx
+++ src/components/organisms/ExperiencePannel.tsx
@@ -54,7 +54,7 @@ const LeftExperiencePannel = (props: SubExperiencePannelProps) => {
     <div
       className="relative translate-x-[1px]  border-r-2 border-solid border-[#4c5870] pb-6 pl-6
         pt-10 before:absolute before:top-1 before:block before:pt-2 before:text-sm before:font-bold 
-          before:content-[attr(date-is)]  after:absolute after:right-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px] after:rotate-[135]
+          before:content-[attr(date-is)]  after:absolute after:right-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px] after:rotate-[135deg]
           after:rounded-t-xl after:rounded-bl-xl  after:border-2 after:border-solid  after:border-[#4c5870] after:bg-white after:content-[''] md:mr-[50%] md:pr-6"
       date-is={date}
     >
