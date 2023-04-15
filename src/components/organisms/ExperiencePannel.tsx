1cd2a8460deea5b025b3cc3d1495c6371549432a
hirakuuuu <m.hiraku1129@gmail.com>
2023-04-15 18:09:14 +0900

    [fix] 角度調整

--- src/components/organisms/ExperiencePannel.tsx
+++ src/components/organisms/ExperiencePannel.tsx
@@ -37,7 +37,7 @@ const RightExperiencePannel = (props: SubExperiencePannelProps) => {
     <div
       className="relative -translate-x-[1px] border-l-2  border-solid border-[#4c5870] pb-6 pl-6 pt-10
         before:absolute before:left-6 before:top-1 before:block before:pt-2 before:text-sm before:font-bold 
-          before:content-[attr(date-is)]  after:absolute after:left-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px] after:rotate-45
+          before:content-[attr(date-is)]  after:absolute after:left-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px] after:-rotate-45
            after:rounded-t-xl  after:rounded-bl-xl after:border-2  after:border-solid after:border-[#4c5870]
           after:bg-white after:content-[''] md:ml-[50%] md:pr-6 "
       date-is={date}
@@ -52,9 +52,9 @@ const LeftExperiencePannel = (props: SubExperiencePannelProps) => {
   const { title, date, detail } = props;
   return (
     <div
-      className="relative  translate-x-[1px]  border-r-2 border-solid border-[#4c5870] pb-6 pl-6
-        pt-10 before:absolute before:top-1 before:block before:pt-2 before:text-sm before:font-bold 
-          before:content-[attr(date-is)]  after:absolute after:right-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px] after:rotate-45
+      className="after:rotate-135  relative  translate-x-[1px] border-r-2 border-solid border-[#4c5870] pb-6
+        pl-6 pt-10 before:absolute before:top-1 before:block before:pt-2 before:text-sm 
+          before:font-bold  before:content-[attr(date-is)] after:absolute after:right-[-11px] after:top-[54px] after:block after:h-[20px] after:w-[20px]
           after:rounded-t-xl after:rounded-bl-xl  after:border-2 after:border-solid  after:border-[#4c5870] after:bg-white after:content-[''] md:mr-[50%] md:pr-6"
       date-is={date}
     >
